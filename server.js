14498970f50553521be39a26a870136a2fa85c3b
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-02-17 15:03:45 -0500

    Added to do list to server js

---
+++ server.js
@@ -0,0 +1,37 @@
+//to do
+// start app -> prompts: 
+//view
+//  all depts
+//  all roles
+//  all employees
+//add
+//   a department
+//  a role
+//  an employee
+//update an employee role
+
+// choose all depts -> table w/ dept names & dept ids
+
+// choose all roles -> table w/ job title, role id, dept, salary
+
+// choose all employees -> 
+// table w/ employee ids, first name, last name, job title, dept, salaries, managers
+
+// choose add dept -> prompts:
+// enter name of dept
+// it is added to database
+
+// choose add role -> prompts:
+// enter name of role
+// enter salary
+// dept for that role
+// it is added to database
+
+// choose add employee -> prompts: 
+// enter first name
+// enter last name
+// enter role
+// enter manager
+// it is added to database
+
+// choose update employee role -> prompted to select employee to update and their role is updated
